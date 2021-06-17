0d2a996a4a71a3bad162f9c4a48efecf47d1a2c9
root <root@DESKTOP-IE8KBFV.localdomain>
2021-06-17 14:09:35 +0900

    update

--- node/client_tcp.js
+++ node/client_tcp.js
@@ -1,35 +1,38 @@
-var net = require('net');
-function getConnection(connName){
-  var client = net.connect({port: 8107, host:'172.31.119.7'}, function() {
-    this.setTimeout(500);
-    this.setEncoding('utf8');
-    this.on('data', function(data) { 
-    console.log(data)
-      this.end();
-    });
-    this.on('end', function() {
-    });
-    this.on('error', function(err) {
-    });
-    this.on('timeout', function() {
-    });
-    this.on('close', function() {
-    });
-  });
-  return client;
+var net = require('net');
+
+function getConnection(connName){
+    var client = net.connect({port: 8107, host:'172.31.119.7'}, function() {
+        console.log(connName);
+        this.setTimeout(500); //ms(밀리 세컨드)
+        this.setEncoding('utf8');
+        this.on('data', function(data) { // on << data라는 이벤트가 오면 어떻게 처리 할거다!
+            console.log(data)
+            this.end();
+        });
+        this.on('end', function() {
+        });
+        this.on('error', (err) => {  // 'functuin()'를 '() =>' 로 써도 됨
+            console.log(err)
+        });
+        this.on('timeout', function() {
+        });
+        this.on('close', function() {
+        });
+    });
+    return client; 
 }
 
-function writeData(socket, data){
-  var success = !socket.write(data);
-  if (!success){
-    (function(socket, data){
-      socket.once('drain', function(){
-        writeData(socket, data);
-      });
-    })(socket, data);
-  }
+function writeData(socket, data){
+    var success = !socket.write(data);
+    if (!success){
+        (function(socket, data){
+            socket.once('drain', function(){
+                writeData(socket, data);
+            });
+        })(socket, data);
+    }
 }
-var Alice = getConnection("Alice's Connection");
-var Bob = getConnection("Bob's Connection");
-writeData(Alice, "Alice|Hi, Server");
-writeData(Bob, "Bob|Hello, Server");
+var Alice = getConnection("Alice's Connection");
+var Bob = getConnection("Bob's Connection");
+writeData(Alice, "Alice|Hi, Server");
+writeData(Bob, "Bob|Hello, Server");
--- node/server_tcp.js
+++ node/server_tcp.js
@@ -1,34 +1,37 @@
-var net = require('net');
-var server = net.createServer(function(client) {
-  client.setTimeout(500);
-  client.setEncoding('utf8');
-  client.on('data', function(data) {
-    writeData(client, 'Sending: ' + data.toString());
-  });
-  client.on('end', function() {
-    server.getConnections(function(err, count){
-    });
-  });
-  client.on('error', function(err) {
-  });
-  client.on('timeout', function() {
-  });
+var net = require('net');
+    var server = net.createServer(function(client) {
+        console.log("hi")
+    client.setTimeout(500);
+    client.setEncoding('utf8');
+    client.on('data', function(data) {
+        username = data.split("|")[0]
+        writeData(client, 'Sending: nice to meet you ' + username);
+    });
+    client.on('end', function() {
+        server.getConnections(function(err, count){
+        });
+    });
+    client.on('error', function(err) {
+        console.log(err)
+    });
+    client.on('timeout', function() {
+    });
 });
 
-server.listen(8107, function() {
-  server.on('close', function(){
-  });
-  server.on('error', function(err){
-  });
+server.listen(8107, function() {
+    server.on('close', function(){
+    });
+    server.on('error', function(err){
+    });
 });
-function writeData(socket, data){
-  var success = !socket.write(data);
-  if (!success){
-    (function(socket, data){
-      socket.once('drain', function(){
-        writeData(socket, data);
-      });
-    })(socket, data);
-  }
+function writeData(socket, data){
+    var success = !socket.write(data);
+    if (!success){
+        (function(socket, data){
+            socket.once('drain', function(){
+                writeData(socket, data);
+        });
+    })(socket, data);
+    }
 }
-console.log('123')
+console.log("server on")
